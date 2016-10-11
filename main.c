3ccf3d78ec3455cd52236fb497772a72e7cb7f01
Antoine Cronier <antoine.cronier@tactfactory.com>
2016-10-11 16:44:53 +0200

    update

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <windows.h>
 
-#define DESTROYERID 2
 
 int tp1(){
     int j1 = 5;
@@ -426,13 +426,6 @@ int Secret(int boucle){
         scanf("%d[0-9]",&clef);
         fflush(stdin);
 
-        /*for(i = 0; i < 100; i++){
-            if(saisi[i] != 0){
-                printf("%c ",saisi[i]);
-            }
-        }
-        printf("\n");*/
-
         for(i = 0; i < 100; i++){
             if((int)saisi[i] != 0){
                 result[i] = (int)saisi[i]*(int)saisi[i]-(int)saisi[i]+clef;
@@ -479,20 +472,6 @@ int SecretReverse(int boucle){
             correspond[i][1] = ((i+33)*(i+33))-(i+33)+clef;
         }
 
-        /*printf("\n");
-        for(i = 0; i < 93; i++){
-            printf("%d ",correspond[i][0]);
-        }
-        printf("\n");
-        for(i = 0; i < 93; i++){
-            printf("%c ",correspond[i][0]);
-        }
-        printf("\n");
-        for(i = 0; i < 93; i++){
-            printf("%d ",correspond[i][1]);
-        }
-        printf("\n");*/
-
         for(i = 0; i < 100; i++){
             for(j = 0; j < 93; j++){
                 if(result[i] == correspond[j][1]){
@@ -523,7 +502,7 @@ int SecretAndUnsecret(){
 
 
 #define MAP_H 30
-#define MAP_W 30
+#define MAP_W 50
 #define MAP_ITEMS 3
 #define MAP_SOL 0
 #define MAP_MOVE_TO_PAID 1
@@ -531,10 +510,12 @@ int SecretAndUnsecret(){
 #define PLAYER_MOVE_POINT 10
 #define ID_J1 1
 #define ID_J2 2
+#define ID_J3 3
+#define ID_J4 4
 #define HERBE 1
 #define MOVE_HERBE 1
 #define SABLE 2
-#define MOVE_SABLE 2
+#define MOVE_SABLE 3
 #define ROCHER 3
 #define MOVE_ROCHER 5
 #define PLAYER_STATS 4
@@ -558,17 +539,25 @@ int doRand(int startVal, int endVal){
     }
 }
 
-int PrintTableau(int map[MAP_H][MAP_W][MAP_ITEMS],int item){
+void PrintTableau(int map[MAP_H][MAP_W][MAP_ITEMS],int item){
     int i,j;
-    for(i = 0;i < MAP_H; i++){
-        for(j = 0;j < MAP_W; j++){
-            printf("%d",map[i][j][item]);
+    for(i = -1;i < MAP_H+1; i++){
+        for(j = -1;j < MAP_W+1; j++){
+            if(i == -1 || i == MAP_H || j == -1 || j == MAP_W){
+                printf("#");
+            }else{
+                if(map[i][j][item]!=0){
+                    printf("%d",map[i][j][item]);
+                }else{
+                    printf(" ");
+                }
+            }
         }
         printf("\n");
     }
 }
 
-int JeuRencontreInit(int map[MAP_H][MAP_W][MAP_ITEMS]){
+void JeuRencontreInit(int map[MAP_H][MAP_W][MAP_ITEMS]){
     int i,j;
     for(i = 0;i < MAP_H; i++){
         for(j = 0;j < MAP_W; j++){
@@ -601,39 +590,115 @@ void JeuRencontrePlacementJoueur(int player[PLAYER_STATS],int map[MAP_H][MAP_W][
 }
 
 void JeuRencontreDeplacement(int player[PLAYER_STATS],int destination[PLAYER_STATS],int map[MAP_H][MAP_W][MAP_ITEMS]){
-    printf("deplacement\n");
+    //printf("deplacement\n");
     while(player[MP]>0 && (player[X] != destination[X] || player[Y] != destination[Y])){
-        printf("MP = %d\n", player[MP]);
+        //waitFor(1.5);
+        system("cls");
+        MessageBeep(MB_ICONWARNING);
+        //printf("MP = %d\n", player[MP]);
         if(destination[X]>player[X] && map[player[X]+1][player[Y]][MAP_MOVE_TO_PAID] <= player[MP]){
+            map[player[X]][player[Y]][MAP_PLAYER] = 0;
             map[player[X]+1][player[Y]][MAP_PLAYER] = player[ID];
             player[MP] -= map[player[X]+1][player[Y]][MAP_MOVE_TO_PAID];
             player[X]++;
         }else if(destination[X]<player[X] && map[player[X]-1][player[Y]][MAP_MOVE_TO_PAID] <= player[MP]){
+            map[player[X]][player[Y]][MAP_PLAYER] = 0;
             map[player[X]-1][player[Y]][MAP_PLAYER] = player[ID];
             player[MP] -= map[player[X]-1][player[Y]][MAP_MOVE_TO_PAID];
             player[X]--;
         }else if(destination[Y]>player[Y] && map[player[X]][player[Y]+1][MAP_MOVE_TO_PAID] <= player[MP]){
+            map[player[X]][player[Y]][MAP_PLAYER] = 0;
             map[player[X]][player[Y]+1][MAP_PLAYER] = player[ID];
             player[MP] -= map[player[X]][player[Y]+1][MAP_MOVE_TO_PAID];
             player[Y]++;
         }else if(destination[Y]<player[Y] && map[player[X]][player[Y]-1][MAP_MOVE_TO_PAID] <= player[MP]){
+            map[player[X]][player[Y]][MAP_PLAYER] = 0;
             map[player[X]][player[Y]-1][MAP_PLAYER] = player[ID];
             player[MP] -= map[player[X]][player[Y]-1][MAP_MOVE_TO_PAID];
             player[Y]--;
         }else{
             player[MP] = 0;
         }
+        PrintTableau(map,MAP_PLAYER);
     }
+    //printf("MP = %d\n", player[MP]);
+}
+
+void JeuRencontreSave(int map[MAP_H][MAP_W][MAP_ITEMS]){
+    /*FILE *fptr=fopen("./save.txt","wb");
+    fwrite(map , sizeof(int) , MAP_H*MAP_W*MAP_ITEMS , fptr);
+    fclose(fptr);*/
+    FILE * fp;
+
+    fp = fopen ("./file.txt", "w+");
+    int i,j,k;
+    for(i = 0;i < MAP_H; i++){
+        for(j = 0;j < MAP_W; j++){
+            for(k = 0;k < MAP_ITEMS; k++){
+                fprintf(fp, "%c", (char)map[i][j][k]);
+            }
+        }
+    }
+
+    fclose(fp);
+}
+
+void JeuRencontreLoad(int map[MAP_H][MAP_W][MAP_ITEMS]){
+    /*FILE *ifp = fopen("/save.txt", "rb");
+    fread(map, sizeof(int), MAP_H*MAP_W*MAP_ITEMS, ifp);*/
+    FILE *fp;
+    int i,j,k;
+    char c;
+
+    fp = fopen("./file.txt","r");
+    for(i = 0;i < MAP_H; i++){
+        for(j = 0;j < MAP_W; j++){
+            for(k = 0;k < MAP_ITEMS; k++){
+                c = fgetc(fp);
+                if( feof(fp) )
+                {
+                    break;
+                }
+                map[i][j][k]=(int)c;
+            }
+        }
+    }
+    fclose(fp);
+
+    for(i = 0;i < MAP_H; i++){
+        for(j = 0;j < MAP_W; j++){
+            map[i][j][MAP_PLAYER]=0;
+        }
+    }
+
+
+    /*fp = fopen("./file.txt","r");
+    while(1)
+    {
+        c = fgetc(fp);
+        if( feof(fp) )
+        {
+            break;
+        }
+        printf("%c ", c);
+    }
+    fclose(fp);*/
 }
 
 void JeuRencontre(){
+    int save = 1;
     int player1[PLAYER_STATS];
     player1[ID] = ID_J1;
     int player2[PLAYER_STATS];
     player2[ID] = ID_J2;
+    /*int player3[PLAYER_STATS];
+    player3[ID] = ID_J3;
+    int player4[PLAYER_STATS];
+    player4[ID] = ID_J4;*/
     int map[MAP_H][MAP_W][MAP_ITEMS] = {0};
 
     JeuRencontreInit(map);
+    //JeuRencontreLoad(map);
     PrintTableau(map,MAP_SOL);
     printf("////////////////////////////////////////\n");
     PrintTableau(map,MAP_MOVE_TO_PAID);
@@ -641,18 +706,41 @@ void JeuRencontre(){
     getchar();
     JeuRencontrePlacementJoueur(player1,map);
     JeuRencontrePlacementJoueur(player2,map);
+/*    JeuRencontrePlacementJoueur(player3,map);
+    JeuRencontrePlacementJoueur(player4,map);*/
     PrintTableau(map,MAP_PLAYER);
     printf("////////////////////////////////////////\n");
     getchar();
-    while(player1[X] != player2[X] && player1[Y] != player2[Y]){
-        player1[MP] = PLAYER_MOVE_POINT;
+
+    while((player1[X] != player2[X] || player1[Y] != player2[Y])
+          /*|| (player1[X] != player3[X] && player1[Y] != player3[Y])
+          || (player1[X] != player4[X] && player4[Y] != player4[Y])*/){
+        if(save){
+            printf("saving\n");
+            JeuRencontreSave(map);
+        }
+        player1[MP] = PLAYER_MOVE_POINT;//*2;
         player2[MP] = PLAYER_MOVE_POINT;
+        /*player3[MP] = PLAYER_MOVE_POINT;
+        player4[MP] = PLAYER_MOVE_POINT;*/
         JeuRencontreDeplacement(player1,player2,map);
         JeuRencontreDeplacement(player2,player1,map);
-        PrintTableau(map,MAP_PLAYER);
-        printf("////////////////////////////////////////\n");
-        getchar();
+        /*JeuRencontreDeplacement(player3,player1,map);
+        JeuRencontreDeplacement(player4,player1,map);*/
+
+        /*int destination[PLAYER_STATS];
+        destination[X]=doRand(0,MAP_H-1);
+        destination[Y]=doRand(0,MAP_W-1);
+
+        JeuRencontreDeplacement(player1,destination,map);*/
+
+        //PrintTableau(map,MAP_PLAYER);
+        //printf("////////////////////////////////////////\n");
+        //getchar();
     }
+    //PrintTableau(map,MAP_PLAYER);
+    //printf("////////////////////////////////////////\n");
+    MessageBeep(MB_OK);
     printf("Gagne\n");
 }
 
@@ -671,6 +759,224 @@ void useFonction(){
     printf("a = %d b = %d r = %d\n",a,b,r);
 }
 
+#define CUVE_SP95_1_MAX 100
+#define CUVE_SP95_2_MAX 120
+#define CUVE_G_1_MAX 150
+#define CUVE_G_2_MAX 80
+#define CUVE_SP98_1_MAX 100
+
+#define CAMION_SP95_MAX 80
+#define CAMION_SP98_MAX 60
+#define CAMION_G_MAX 150
+
+void AfficherCuve(int cuve, int max){
+    int i,j;
+    for(i=0;i<cuve;i++){
+        printf("|");
+    }
+    printf(" -> %f pourcent\n",(float)cuve/(float)max*100);
+}
+
+void Remplir(int* cuve, int* camion, int max){
+    system("cls");
+    while(*cuve < max && *camion > 0){
+        *cuve = *cuve + 1;
+        *camion = *camion - 1;
+    }
+    AfficherCuve(*cuve,max);
+    getchar();
+}
+
+void Vider(int* cuve){
+    *cuve = *cuve - doRand(0,*cuve);
+}
+
+void Ravitaillement(int* camionG,int* camionSP95,int* camionSP98){
+    int saisi = -1;
+    while(saisi){
+        system("cls");
+        printf("Etat camionG : %f pourcent soit %d litre\n",(float)*camionG /(float)CAMION_G_MAX*100,*camionG);
+        printf("1 : pour ravitailler\n");
+        printf("Etat camionSP95 : %f pourcent soit %d litre\n",(float)*camionSP95/(float)CAMION_SP95_MAX*100,*camionSP95);
+        printf("2 : pour ravitailler\n");
+        printf("Etat camionSP98 : %f pourcent soit %d litre\n",(float)*camionSP98/(float)CAMION_SP98_MAX*100,*camionSP98);
+        printf("3 : pour ravitailler\n");
+        scanf("%d",&saisi);
+        switch(saisi){
+            case 0:
+                break;
+            case 1:
+                *camionG = CAMION_G_MAX;
+                printf("Camion rempli\n");
+                break;
+            case 2:
+                *camionSP95 = CAMION_SP95_MAX;
+                printf("Camion rempli\n");
+                break;
+            case 3:
+                *camionSP98 = CAMION_SP98_MAX;
+                printf("Camion rempli\n");
+                break;
+            default :
+                printf("Erreur de saisi\n");
+                break;
+        }
+    }
+}
+
+void Stats(int* camionG,int* camionSP95,int* camionSP98,
+           int* cuveG1,int* cuveG2,int* cuveSP951,int* cuveSP952,int* cuveSP981){
+    AfficherCuve(*cuveG1,CUVE_G_1_MAX);
+    printf("Cuve G 1 :%d\n\n",*cuveG1);
+    AfficherCuve(*cuveG2,CUVE_G_2_MAX);
+    printf("Cuve G 2 :%d\n\n",*cuveG2);
+    AfficherCuve(*cuveSP951,CUVE_SP95_1_MAX);
+    printf("Cuve SP95 1 :%d\n\n",*cuveSP951);
+    AfficherCuve(*cuveSP952,CUVE_SP95_2_MAX);
+    printf("Cuve SP95 2 :%d\n\n",*cuveSP952);
+    AfficherCuve(*cuveSP981,CUVE_SP98_1_MAX);
+    printf("Cuve SP98 1 :%d\n\n",*cuveSP981);
+
+    AfficherCuve(*camionG,CAMION_G_MAX);
+    printf("Camion G :%d\n\n",*camionG);
+    AfficherCuve(*camionSP95,CAMION_SP95_MAX);
+    printf("Camion SP95 :%d\n\n",*camionSP95);
+    AfficherCuve(*camionSP98,CAMION_SP98_MAX);
+    printf("Camion SP98 :%d\n\n",*camionSP98);
+    getchar();
+}
+
+void SaveCuve(int* camionG,int* camionSP95,int* camionSP98,
+           int* cuveG1,int* cuveG2,int* cuveSP951,int* cuveSP952,int* cuveSP981){
+    FILE* ftp = fopen("./cuveBKP","w+");
+    fprintf(ftp,"%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d",*camionG,*camionSP95,*camionSP98
+            ,*cuveG1,*cuveG2,*cuveSP951,*cuveSP952,*cuveSP981);
+    fclose(ftp);
+}
+
+void LoadCuve(int* camionG,int* camionSP95,int* camionSP98,
+           int* cuveG1,int* cuveG2,int* cuveSP951,int* cuveSP952,int* cuveSP981){
+    FILE* fptr = fopen("./cuveBKP","r");
+    *camionG = (int)fgetc(fptr);
+    *camionSP95 = (int)fgetc(fptr);
+    *camionSP98 = (int)fgetc(fptr);
+    *cuveG1 = (int)fgetc(fptr);
+    *cuveG2 = (int)fgetc(fptr);
+    *cuveSP951 = (int)fgetc(fptr);
+    *cuveSP952 = (int)fgetc(fptr);
+    *cuveSP981 = (int)fgetc(fptr);
+    fclose(fptr);
+}
+
+void Cuve(){
+    int cuveSP951 = 2;
+    int cuveSP952 = 0;
+    int cuveSP981 = 4;
+    int cuveG1 = 3;
+    int cuveG2 = 5;
+    int camionSP95 = CAMION_SP95_MAX;
+    int camionSP98 = CAMION_SP98_MAX;
+    int camionG = CAMION_G_MAX;
+    char question = ' ';
+
+    while(question != 'Y' && question != 'N'){
+        printf("Voulez vous charger la derniere sauvegarde (Y/N)\n");
+        scanf("%c[YN]",&question);
+        fflush(stdin);
+        system("cls");
+    }
+
+    if(question == 'Y'){
+        LoadCuve(&camionG,&camionSP95,&camionSP98,&cuveG1,&cuveG2,&cuveSP951,&cuveSP952,&cuveSP981);
+    }
+
+    int i;
+    for(i=0;i<10;i++){
+        int saisi=-1;
+        Stats(&camionG,&camionSP95,&camionSP98,&cuveG1,&cuveG2,&cuveSP951,&cuveSP952,&cuveSP981);
+        system("cls");
+        while(saisi){
+            system("cls");
+            printf("Jour %d sur 10\n",i+1);
+            printf("Selectionner cuve a remplir (0 pour arreter)\n");
+            printf("1 : Cuve SP95 1\n");
+            printf("2 : Cuve SP95 2\n");
+            printf("3 : Cuve SP98 1\n");
+            printf("4 : Cuve G 1\n");
+            printf("5 : Cuve G 2\n");
+            printf("6 : Ravitailler un camion\n");
+            printf("7 : Afficher le status de la station\n");
+            printf("8 : Save\n");
+            scanf("%d",&saisi);
+            fflush(stdin);
+
+            switch(saisi){
+                case 0:
+                    break;
+                case 1:
+                    Remplir(&cuveSP951,&camionSP95,CUVE_SP95_1_MAX);
+                    break;
+                case 2:
+                    Remplir(&cuveSP952,&camionSP95,CUVE_SP95_2_MAX);
+                    break;
+                case 3:
+                    Remplir(&cuveSP981,&camionSP98,CUVE_SP98_1_MAX);
+                    break;
+                case 4:
+                    Remplir(&cuveG1,&camionG,CUVE_G_1_MAX);
+                    break;
+                case 5:
+                    Remplir(&cuveG2,&camionG,CUVE_G_2_MAX);
+                    break;
+                case 6:
+                    Ravitaillement(&camionG,&camionSP95,&camionSP98);
+                    break;
+                case 7:
+                    Stats(&camionG,&camionSP95,&camionSP98,&cuveG1,&cuveG2,&cuveSP951,&cuveSP952,&cuveSP981);
+                    break;
+                case 8:
+                    SaveCuve(&camionG,&camionSP95,&camionSP98,&cuveG1,&cuveG2,&cuveSP951,&cuveSP952,&cuveSP981);
+                    break;
+                default :
+                    printf("Erreur de saisi\n");
+                    break;
+            }
+        }
+
+        Vider(&cuveG1);
+        Vider(&cuveG2);
+        Vider(&cuveSP951);
+        Vider(&cuveSP952);
+        Vider(&cuveSP981);
+        getchar();
+    }
+}
+
+void FilerWrite(){
+    FILE* ftp = fopen("./test1.txt","a+");
+    int i = 0;
+    while(i < 1000){
+        fprintf(ftp,"Hello world %d\n",i);
+        i++;
+    }
+    fclose(ftp);
+}
+
+void FilerRead(){
+    FILE* fptr = fopen("./test1.txt","r");
+    char c;
+    while(1)
+    {
+        c = fgetc(fptr);
+        if ( feof(fptr) )
+        {
+            break;
+        }
+        printf ("%c", c);
+    }
+    fclose(fptr);
+}
+
 int main()
 {
     //Square2();
@@ -680,5 +986,9 @@ int main()
     Multiplication();
     //SecretAndUnsecret();
     //JeuRencontre();
+    //Cuve();
+    //FilerWrite();
+    //FilerRead();
+
     return 0;
 }
